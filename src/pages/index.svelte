672f4fd7f109b08e1058cb0e942eb99de25f8f06
inoue <ht8sk13@icloud.com>
2023-02-03 23:54:57 +0900

    initial commit

---
+++ src/pages/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { metatags } from '@roxi/routify'
+
+    metatags.title = 'My Routify app'
+    metatags.description = 'Description coming soon...'
+</script>
+
+<h1 class="text-3xl bg-slate-500 text-white px-2 py-4">Hello,World!</h1>
